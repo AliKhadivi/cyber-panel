ac497bef723526a615d7ff3a4a48993cce0ca03c
Ali Khadivi <74895885+AliKhadivi@users.noreply.github.com>
2022-03-29 01:53:39 +0430

    Update dependency.sh

--- dependency.sh
+++ dependency.sh
@@ -4,9 +4,6 @@ yum install git wget curl  ca-certificates -y
 
 curl -o /root/.acme.sh/acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
 
-rm -f cyberpanel.sh
-rm -f install.tar.gz
-curl --silent -o cyberpanel.sh "https://cyberpanel.sh/?dl&$SERVER_OS" 2>/dev/null
 chmod +x cyberpanel.sh
 ./cyberpanel.sh $@
 
